a7aaf23c341c29be34a4a2c3d251267116f1d1b2
Ilari Vallivaara <ilari.vallivaara@gmail.com>
2021-10-28 11:42:15 +0100

    Minor refactoring

--- ancestry/src/helpers/Debug.scala
+++ ancestry/src/helpers/Debug.scala
@@ -10,11 +10,15 @@ object Debug {
     val INTENT_PER_LEVEL = 4
     def rec(tree: Tree[T], intendation: Int): String = {
       val prefix =
-        if (intendation > 0) " " * (intendation - INTENT_PER_LEVEL) + "\\" + "." * (INTENT_PER_LEVEL - 1) else ""
-      val tag = prefix + toString(tree) + s" (${tree.parent.map { p => toString(p) }.getOrElse("root")})" + "\n"
+        if (intendation > 0)
+          " " * (intendation - INTENT_PER_LEVEL) + "\\" + "." * (INTENT_PER_LEVEL - 1)
+        else ""
+      val tag =
+        prefix + toString(tree) + s" (${tree.parent.map { p => toString(p) }.getOrElse("root")})" + "\n"
       tree match {
-        case _: Leaf[T]   => tag
-        case b: Branch[T] => tag + b.children.map(c => rec(c, intendation + INTENT_PER_LEVEL)).fold("")(_ + _)
+        case _: Leaf[T] => tag
+        case b: Branch[T] =>
+          tag + b.children.map(c => rec(c, intendation + INTENT_PER_LEVEL)).fold("")(_ + _)
       }
     }
 
